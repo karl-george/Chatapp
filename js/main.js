356e88e4ed97ba80f87ab13446c3ee1425028073
Karl George <karlgdev@gmail.com>
2022-02-02 14:33:28 +0100

    Created info block component

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+if (window.innerWidth < 768) {
+  collapsibles.forEach((item) =>
+    item.addEventListener("click", function () {
+      this.classList.toggle("collapsible-expanded");
+    })
+  );
+}
