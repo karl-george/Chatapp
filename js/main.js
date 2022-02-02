e1d3cf85482b34bb303a434e3437cfdf08b350a6
Karl George <karlgdev@gmail.com>
2022-02-02 15:26:17 +0100

    Working on nav bar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+// const collapsibles = document.querySelectorAll(".collapsible");
+// if (window.innerWidth < 768) {
+//   collapsibles.forEach((item) =>
+//     item.addEventListener("click", function () {
+//       this.classList.toggle("collapsible-expanded");
+//     })
+//   );
+// }
+
 const collapsibles = document.querySelectorAll(".collapsible");
-if (window.innerWidth < 768) {
-  collapsibles.forEach((item) =>
-    item.addEventListener("click", function () {
-      this.classList.toggle("collapsible-expanded");
-    })
-  );
-}
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible-expanded");
+  })
+);
